dfa3c2f080ef9420de0690a247b1e2fab80cd57b
Salo Charabati <salochara@gmail.com>
2019-06-18 16:00:38 -0500

    bolierplate for Angular App

--- FirstAngularApp/AngularApp/src/app/app.component.ts
+++ FirstAngularApp/AngularApp/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+// decoradores... elementos que describen a nuestro componenete.
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
