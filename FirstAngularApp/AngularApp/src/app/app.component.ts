573954d07633e3f4f83db9338ed9c8debef87919
Salo Charabati <salochara@gmail.com>
2019-06-18 20:34:01 -0500

    genrated two components and played around with html

--- FirstAngularApp/AngularApp/src/app/app.component.ts
+++ FirstAngularApp/AngularApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { Alumno } from './titulo2/titulo2.component';
 
 // decoradores... elementos que describen a nuestro componenete.
 @Component({
@@ -6,6 +7,23 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'AngularApp';
+// este es el title que se renderea en ell app.component.html
+export class AppComponent implements OnInit{
+  title = 'PracticaClaseWeb';
+  instanciaAlumno: Alumno = new Alumno();
+  calificacion = 50;
+
+  leDiClick($event){
+    this.title = 'Le di click';
+    console.log($event);
+    console.log('Class name es (entrando con el array !): ' + $event.path[0].className);
+  }
+
+
+
+// esto es porque importe la clase Alumno del componente Titulo2
+ngOnInit(){
+  this.instanciaAlumno.nombre = 'Salo';
 }
+}
+
--- FirstAngularApp/AngularApp/src/app/app.module.ts
+++ FirstAngularApp/AngularApp/src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TituloComponent } from './titulo/titulo.component';
+import { Titulo2Component } from './titulo2/titulo2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TituloComponent,
+    Titulo2Component
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] // el bootstrap es el que agarra el index.html.. ahroita solo ve el App Component...
+  // ya del app.component.html se pueden importar mas componentes
 })
 export class AppModule { }
